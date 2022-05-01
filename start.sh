0daf8df01679405b8c91f94654d49e6605af70e6
Dmitry Misharov <dmisharo@redhat.com>
2022-05-01 22:22:34 +0200

    Fixed start script for Fedora 35

--- start.sh
+++ start.sh
@@ -21,8 +21,8 @@ else
     exit 1
 fi
 
-$CONTAINER_RUNTIME run -it --rm -w /app -v .:/app:z quay.io/redhatqe/cypress:9.6.0 bash -c "yarn install"
-CONTAINER=$($CONTAINER_RUNTIME run -d --rm -p 5999:5999 -p 8080:3000 -e VNC_GEOMETRY=1920x1080 --shm-size=2g -w /app -v .:/app quay.io/redhatqe/cypress:9.6.0 bash -c "yarn dev")
+$CONTAINER_RUNTIME run -it -u 0 --rm -w /app -v .:/app:z quay.io/redhatqe/cypress:9.6.0 bash -c "yarn install"
+CONTAINER=$($CONTAINER_RUNTIME run -d -u 0 --rm -p 5999:5999 -p 8080:3000 -e VNC_GEOMETRY=1920x1080 --shm-size=2g -w /app -v .:/app:z quay.io/redhatqe/cypress:9.6.0 bash -c "yarn dev")
 $CONTAINER_RUNTIME exec -t $CONTAINER bash -c "startcypress open" &
 
 cat
