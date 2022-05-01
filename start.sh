0e21ac46a98d25b7e34b5b00426e81e30fbcfd23
Dmitry Misharov <dmisharo@redhat.com>
2022-05-01 21:03:06 +0200

    Added cleanup routine into the start script

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+cleanup() {
+    echo ""
+    echo "Cleaning up..."
+    if $CONTAINER_RUNTIME rm -f $CONTAINER > /dev/null; then
+        echo "Cleanup successful"
+    else
+        echo "Cleanup failed"
+    fi
+}
+
+trap cleanup SIGINT
+
 if command -v docker > /dev/null 2>&1; then
     CONTAINER_RUNTIME=docker
 elif command -v podman > /dev/null 2>&1; then
@@ -10,5 +22,7 @@ else
 fi
 
 $CONTAINER_RUNTIME run -it --rm -w /app -v .:/app quay.io/redhatqe/cypress:9.6.0 bash -c "yarn install"
-CONTAINER=$($CONTAINER_RUNTIME run -d --rm -p 5999:5999 -e VNC_GEOMETRY=1920x1080 --shm-size=2g -w /app -v .:/app quay.io/redhatqe/cypress:9.6.0 bash -c "yarn dev")
-$CONTAINER_RUNTIME exec -it $CONTAINER bash -c "startcypress open"
+CONTAINER=$($CONTAINER_RUNTIME run -d --rm -p 5999:5999 -p 8080:3000 -e VNC_GEOMETRY=1920x1080 --shm-size=2g -w /app -v .:/app quay.io/redhatqe/cypress:9.6.0 bash -c "yarn dev")
+$CONTAINER_RUNTIME exec -t $CONTAINER bash -c "startcypress open" &
+
+cat
